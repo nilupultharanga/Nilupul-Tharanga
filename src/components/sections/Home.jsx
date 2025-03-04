c3d211b796d1f91eeee32ae99bfd79bcb2246f40
nilupultharanga <nilupultharanga279@gmail.com>
2025-03-04 11:12:29 +0530

    six changed

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -15,12 +15,12 @@ export const Home= ()=>{
                     </p>
                     <div className="flex justify-center space-x-4">
                         <a href="#project"
-                        className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-1/2 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+                        className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-1/10 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
                         >
                             View Projects
                         </a>
                         <a href="#contact"
-                        className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-1/2 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+                        className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-1/10 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
                         >
                             Contact Me
                         </a>
